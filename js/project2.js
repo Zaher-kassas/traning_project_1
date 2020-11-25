b9f9d7dc56e756194114b47aecb957fcd94a8103
Zaher-kassas <z1h12323@gmail.com>
2020-11-25 16:28:47 +0200

    add navigatin background and slider test2

--- js/project2.js
+++ js/project2.js
@@ -10,15 +10,18 @@ body.onscroll=()=>
 }
 //Choose section*******************************************************
 let a=["../images/about/about1.webp","../images/about/about2.webp"];
+a.push("../images/about/about3.jpg")
+a.push("../images/about/about4.jpg")
 let x=0;
 function sliderMove()
 {
     console.log(x);
     let photo0=document.querySelector(".choose-img-photo"+x%2)
     let photo1=document.querySelector(".choose-img-photo"+(x+1)%2)
-    console.log(photo1)
     photo0.style.animationName="slider-photo1"
     photo1.style.animationName="slider-photo2"
+    a.push(a.shift())
+    setTimeout(()=>photo0.setAttribute("src",a[0]),4000)
     x++
 }
 setInterval(sliderMove,4000)
